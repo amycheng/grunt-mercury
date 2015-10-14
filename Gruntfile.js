e57a5e89a0cc1d2133d612552a2d08c49948f6b5
Amy Cheng <amy.cheng@brooklynmuseum.org>
2015-10-14 16:42:09 -0400

    removed a few tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,36 +3,21 @@
 
 
 module.exports = function(grunt) {
-    //CUSTOMIZE OR ADD VARIABLES TO HAVE GRUNT LOOK AT THE CORRECT ASSETS
     var
-    dev_css = '_dev/css',
-    dev_js = '_dev/js',
-    dev_img = '_dev/images',
-    dev_html= '_dev/html',
-    target_css = 'site/css',
-    target_js = 'site/js',
-    target_img = 'site/images',
-    target='site';
+    target="",
+    target_js = "js";
+    target_css = "css";
+
+    var
+    dev_css = "sass";
 
     grunt.initConfig({
-        assemble: { //precompiles HTML from Handlebars Templates
-            html: {
-                options: {
-                    engine: 'handlebars',
-                    partials: dev_html+'/*.hbs',
-                    data: dev_html+'_/data/data.json'
-                },
-                files: {
-                    'site/index.html' : dev_html+'/index.hbs'
-                }
-            }
-        },
-        concat: {
-            javascript: {
-                src:[],
-                dest: target_js+'/script.js'
-            }
-        },
+        // concat: {
+        //     javascript: {
+        //         src:[],
+        //         dest: target_js+'/script.js'
+        //     }
+        // },
         connect: {
             server: {
                 options: {
@@ -41,37 +26,17 @@ module.exports = function(grunt) {
                 }
             }
         },
-        copy: {
-            images: {
-                files: [
-                    {
-                    expand: true,
-                     cwd: ['_dev/images/'],
-                     src: '*',
-                      dest: 'site/images/'} // includes files in path and its subdirs
-                ]
-            }
-        },
         watch: {
             options: {
-                livereload: true,
-                nospawn: false
+                livereload: true
             },
+            // js: {
+            //     files: [target_js+'/*'],
+            //     tasks: ['concat:javascript']
+            // },
             css: {
                 files: [dev_css+'/*'],
                 tasks: ['sass:dev']
-            },
-            js: {
-                files: [dev_js+'/*'],
-                tasks: ['concat:javascript']
-            },
-            images: {
-                files:[dev_img+'/*'],
-                tasks: ['copy:images']
-            },
-            html: {
-                files: [dev_html+'/**/*.hbs'],
-                tasks: ['assemble:html']
             }
         },
         sass: { //compile CSS from SASS
@@ -79,12 +44,12 @@ module.exports = function(grunt) {
                 options: {
                     style: 'expanded',
                     noCache: false,
-                    lineNumbers: false,
+                    lineNumbers: true,
                     compass: true
                 },
                 files: {
-                    'site/css/style.css': [
-                        dev_css+'/*.scss'
+                    'css/style.css': [
+                        dev_css+'/styles.scss'
                     ]
                 }
             }
@@ -92,11 +57,9 @@ module.exports = function(grunt) {
 
     });
 
-    grunt.loadNpmTasks('assemble');
-    grunt.loadNpmTasks('grunt-contrib-concat');
+    // grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.registerTask('dev', ['assemble','sass','concat','connect', 'watch']);
+    grunt.registerTask('default', ['sass','connect','watch']);
 };
